e02f013883aec5efd953528d30bc8618f26e20b1
Thomas Hodges <thodges@javascriptonline.org>
2019-10-15 23:13:39 -0700

    two more function cases

--- annotations/functions.ts
+++ annotations/functions.ts
@@ -9,3 +9,11 @@ function divide(a:number, b:number):number {
 const multiply = function(a: number, b: number): number {
     return a * b;
 }
+
+const logger = (message: string): void => {         // can optionally return null or undefined
+    console.log(message);
+}
+
+const throwError = (message: string): never => {    // will *never* execute function completely - **rare**
+    throw new Error(message);
+}
